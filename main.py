1af74d430954bab8f539390b8c7f59a12d942e99
Tyler <ArKaes1820@yahoo.com>
2022-09-07 10:40:03 -0700

    successfully replicates a hirst dot painting

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen, colormode
+import random
+
+color_list = [(215, 166, 17), (230, 239, 234), (205, 153, 99),
+              (225, 205, 103), (161, 55, 101), (113, 187, 213), (154, 31, 58), (8, 109, 166), (42, 13, 24),
+              (160, 29, 25), (12, 23, 52), (34, 122, 62), (59, 23, 18), (9, 32, 26), (186, 156, 173), (63, 166, 88),
+              (171, 57, 42), (156, 208, 215), (94, 183, 167), (205, 99, 95), (240, 200, 3), (213, 174, 198),
+              (28, 37, 105), (187, 99, 110), (163, 209, 197), (220, 177, 173), (14, 105, 56), (11, 87, 108),
+              (177, 190, 213), (111, 124, 153), (81, 142, 169), (73, 63, 53)]
+
+
+def random_color():
+    colormode(255)
+    color = random.choice(color_list)
+    timmy.pencolor(color)
+
+
+def set_position(x, y):
+    timmy.penup()
+    timmy.setpos(x, y)
+    timmy.pendown()
+
+
+timmy = Turtle()
+timmy.hideturtle()
+x_pos = -300
+y_pos = -300
+set_position(x_pos, y_pos)
+timmy.speed(0)
+dots = 0
+while dots < 100:
+    for _ in range(10):
+        random_color()
+        timmy.dot(21)
+        timmy.penup()
+        timmy.forward(50)
+        timmy.pendown()
+        dots += 1
+    y_pos += 50
+    set_position(x_pos, y_pos)
+
+
+screen = Screen()
+screen.exitonclick()
